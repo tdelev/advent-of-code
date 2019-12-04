ae445b94acba95f597b666174e0faf9d70bfb1ca
Tomche Delev <tomce.delev@gmail.com>
2019-12-05 00:25:53 +0100

    First commit

---
+++ Day1.hs
@@ -0,0 +1,25 @@
+module Day1 where
+
+toInt :: String -> Int
+toInt s = read s
+
+fuel :: Int -> Int
+fuel mass = mass `div` 3 - 2
+
+massFuel :: Int -> Int
+massFuel mf
+    | fuel mf <= 0   = 0
+    | otherwise = (fuel mf) + massFuel (fuel mf)
+
+fact n
+    | n <= 0    = 1
+    | otherwise = n * fact (n - 1)
+
+
+main :: IO()
+main = do 
+    content <- readFile "day1.txt"
+    let fileLines = lines content
+    let numbers = map toInt fileLines
+    --print $ sum $ map fuel numbers
+    print $ sum $ map massFuel numbers
