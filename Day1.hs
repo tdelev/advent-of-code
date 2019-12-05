6701b31b102496f2518880100ef18bcf3106664f
Tomche Delev <tomche.delev@sorsix.com>
2019-12-05 17:31:17 +0100

    Autoformat code

--- Day1.hs
+++ Day1.hs
@@ -7,19 +7,20 @@ fuel :: Int -> Int
 fuel mass = mass `div` 3 - 2
 
 massFuel :: Int -> Int
-massFuel mf = 
-    let fmf = fuel mf
-    in if fmf <= 0 then 0 else fmf + massFuel fmf
+massFuel mf =
+  let fmf = fuel mf
+   in if fmf <= 0
+        then 0
+        else fmf + massFuel fmf
 
 fact n
-    | n <= 0    = 1
-    | otherwise = n * fact (n - 1)
+  | n <= 0 = 1
+  | otherwise = n * fact (n - 1)
 
-
-main :: IO()
-main = do 
-    content <- readFile "day1.txt"
-    let fileLines = lines content
-    let numbers = map toInt fileLines
+main :: IO ()
+main = do
+  content <- readFile "day1.txt"
+  let fileLines = lines content
+  let numbers = map toInt fileLines
     --print $ sum $ map fuel numbers
-    print $ sum $ map massFuel numbers
+  print $ sum $ map massFuel numbers
--- Day2.hs
+++ Day2.hs
@@ -1,7 +1,7 @@
 module Day2 where
 
-import Data.List.Split
-import Data.Map as M
+import           Data.List.Split
+import           Data.Map        as M
 
 type Code = M.Map Int Int
 
@@ -9,43 +9,45 @@ toInt :: String -> Int
 toInt s = read s
 
 makeProgram :: [Int] -> Code
-makeProgram xs = M.fromList $ zip [0..] xs
+makeProgram xs = M.fromList $ zip [0 ..] xs
 
 updateCode :: Code -> Int -> Int -> Code
 updateCode code position value = M.insert position value code
 
 run :: Code -> Int -> Int
 run code (-1) = code ! 0
-run code   n  = 
-    let step = run_step code n
-    in run (fst step) (snd step)
+run code n =
+  let step = run_step code n
+   in run (fst step) (snd step)
 
 run_step :: Code -> Int -> (Code, Int)
 run_step code cursor = run_operation code (code ! cursor) cursor
 
-
 run_operation :: Code -> Int -> Int -> (Code, Int)
 run_operation code opcode cursor
-    | opcode <= 2 = 
-        let cvalue = value code
-            left = cvalue $ cursor + 1
-            right = cvalue $ cursor + 2
-            position = code ! (cursor + 3)
-            result = if opcode == 1 then left + right else left * right
-        in (updateCode code position result, cursor + 4) 
-    | otherwise = (code, (-1))
+  | opcode <= 2 =
+    let cvalue = value code
+        left = cvalue $ cursor + 1
+        right = cvalue $ cursor + 2
+        position = code ! (cursor + 3)
+        result =
+          if opcode == 1
+            then left + right
+            else left * right
+     in (updateCode code position result, cursor + 4)
+  | otherwise = (code, (-1))
 
 value :: Code -> Int -> Int
 value code position = code ! (code ! position)
 
 main :: IO ()
 main = do
-    input <- readFile "day2.txt"
-    let opcodes = fmap toInt $ splitOn "," input
+  input <- readFile "day2.txt"
+  let opcodes = fmap toInt $ splitOn "," input
     --let opcodes = [1,1,1,4,99,5,6,0,99]
     --print $ makeProgram opcodes
-    let code = makeProgram opcodes
+  let code = makeProgram opcodes
     --print $ code
-    let final_code = Day2.updateCode (Day2.updateCode code 1 12) 2 2
-    print $ run final_code 0
+  let final_code = Day2.updateCode (Day2.updateCode code 1 12) 2 2
+  print $ run final_code 0
     --print $ run code 0
