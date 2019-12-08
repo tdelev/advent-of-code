178a906c1db123aff864ef77acc554d471496d07
Nadica Cuculova <nadica.cuculova@armedia.com>
2019-12-08 20:51:09 +0100

    Solved Day 4

--- Day4.hs
+++ Day4.hs
@@ -1,5 +1,7 @@
 module Day4 where
 
+import Data.List
+
 sixDigit :: Int -> Bool
 sixDigit a = 
     let res = a `div` 100000 
@@ -11,6 +13,15 @@ sameAdjecentDigits a
     | a `mod` 10 == (a `div` 10) `mod` 10 = True
     | otherwise                           = sameAdjecentDigits $ a `div` 10
 
+sameAdjDig :: [Int] -> Bool
+sameAdjDig (a:b:c:d:[]) = a /= b && b == c && c /= d
+
+surround :: [Int] -> [Int]
+surround x = [10] ++ x ++ [10]
+
+fours :: [Int] -> [[Int]]
+fours a = fmap (take 4) (take 5 (tails (surround a)))
+
 neverDecrease :: Int -> Bool
 neverDecrease a
     | a < 10                             = True
@@ -20,12 +31,19 @@ neverDecrease a
 rules :: Int -> Bool
 rules a = (sixDigit a) && (sameAdjecentDigits a) && (neverDecrease a)
 
-differentPasswords :: Int -> Int -> Int
-differentPasswords a b = length $ filter rules [a..b]
-
 digits :: Int -> [Int]
 digits 0 = []
 digits n = (digits $ n `div` 10) ++ [n `mod` 10]
 
+same4 :: Int -> Bool
+same4 a = any sameAdjDig $ fours $ digits a
+
+rules' :: Int -> Bool
+rules' a = (same4 a) && (neverDecrease a)
+
+differentPasswords :: Int -> Int -> Int
+differentPasswords a b = length $ filter rules' [a..b]
+
+
 main :: IO ()
-main = print $ digits 12432 --differentPasswords 347312 805915
+main = print $ differentPasswords 347312 805915
