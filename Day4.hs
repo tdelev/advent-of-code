46fe1cdd98ab99ae387c01ba0bb403386f06d1a9
Nadica Cuculova <nadica.cuculova@armedia.com>
2019-12-08 14:03:06 +0100

    Solved Day 4 part one

---
+++ Day4.hs
@@ -0,0 +1,27 @@
+module Day4 where
+
+sixDigit :: Int -> Bool
+sixDigit a = 
+    let res = a `div` 100000 
+    in res > 0 && res < 10
+
+sameAdjecentDigits :: Int -> Bool
+sameAdjecentDigits a 
+    | a < 10                              = False
+    | a `mod` 10 == (a `div` 10) `mod` 10 = True
+    | otherwise                           = sameAdjecentDigits $ a `div` 10
+
+neverDecrease :: Int -> Bool
+neverDecrease a
+    | a < 10                             = True
+    | a `mod` 10 < (a `div` 10) `mod` 10 = False
+    | otherwise                          = neverDecrease $ a `div` 10
+
+rules :: Int -> Bool
+rules a = (sixDigit a) && (sameAdjecentDigits a) && (neverDecrease a)
+
+differentPasswords :: Int -> Int -> Int
+differentPasswords a b = length $ filter rules [a..b]
+
+main :: IO ()
+main = print $ differentPasswords 347312 805915
