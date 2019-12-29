d79e13c30104a5d5e23fcf31b22fd4605c64b217
Tomche Delev <tomce.delev@gmail.com>
2019-12-29 20:38:56 +0100

    Some more progress

--- Day10.hs
+++ Day10.hs
@@ -6,9 +6,6 @@ type Pos = (Int, Int)
 
 type Space = M.Matrix Char
 
-isAsteroid :: Pos -> Space -> Bool
-isAsteroid pos m = m ! pos == '#'
-
 inLOS :: Pos -> Pos -> Space -> Bool
 inLOS (x1, y1) (x2, y2) m
   | x1 == (x2 - 1) = True
@@ -26,6 +23,39 @@ los space p1 p2 =
     then 1
     else 0
 
+isAsteroid :: Space -> Pos -> Int
+isAsteroid space pos =
+  if space ! pos == '#'
+    then 1
+    else 0
+
+positionsUp (x, y) _ = [(x', y) | x' <- [1 .. (x - 1)]]
+
+positionsDown (x, y) space = [(x', y) | x' <- [(x + 1) .. (nrows space)]]
+
+positionsLeft (x, y) _ = [(x, y') | y' <- [1 .. (y - 1)]]
+
+positionsRight (x, y) space = [(x, y') | y' <- [(y + 1) .. (ncols) space]]
+
+count :: (Pos -> Space -> [Pos]) -> Space -> Pos -> Int
+count genPositions space pos =
+  let total = sum $ fmap (isAsteroid space) $ genPositions pos space
+   in if total > 0
+        then 1
+        else 0
+
+countUp = count positionsUp
+
+countDown = count positionsDown
+
+countLeft = count positionsLeft
+
+countRight = count positionsRight
+
+allDir = [countUp, countDown, countLeft, countRight]
+
+countAll space pos = sum $ fmap (\f -> f space pos) allDir
+
 countLOS :: Pos -> Space -> Int
 countLOS pos space =
   let all = coords space
@@ -39,4 +69,4 @@ main = do
   input <- readFile "day10test.txt"
   let rows = lines input
   let matrix = M.fromLists rows
-  print $ countLOS (1, 1) matrix
+  print $ countAll matrix (3, 5)
