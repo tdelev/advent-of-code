c0b35b8d324b47bd8dc4d849bb8ba576edfcfe79
Tomche Delev <tomche.delev@sorsix.com>
2020-01-08 14:33:35 +0100

    Load in map

--- Day14.hs
+++ Day14.hs
@@ -1,7 +1,7 @@
 module Day14 where
 
 import           Data.List.Split
-import           Data.Map        as M hiding (drop, filter, take)
+import           Data.Map        as M hiding (drop, filter, take, foldl)
 import           Debug.Trace
 
 toPair :: [String] -> ([String], String)
@@ -13,10 +13,13 @@ toInt = read
 
 type Chemical = (Int, String)
 
-type ReactionMap = M.Map Chemical [Chemical]
+type ReactionMap = M.Map String [Chemical]
 
-toReactionMap :: ([String], String) -> ReactionMap
-toReactionMap (xs, key) = M.insert (asChemical key) (fmap asChemical xs) M.empty
+toReactionMap :: ([String], String) -> ReactionMap -> ReactionMap
+toReactionMap (xs, key) m = M.insert (snd $ asChemical key) (fmap asChemical xs) m
+
+asMap :: [([String], String)] -> ReactionMap
+asMap xs = foldl (\m a -> toReactionMap a m) M.empty xs
 
 asChemical :: String -> Chemical
 asChemical s =
@@ -28,5 +31,5 @@ main = do
   input <- readFile "day14test.txt"
   let rows = lines input
   let pairs = fmap (toPair . splitOn " => ") rows
-  let reactions = fmap toReactionMap pairs
+  let reactions = asMap pairs
   print $ reactions
