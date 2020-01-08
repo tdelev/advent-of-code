092bbfb86a51fdf65f5417622f9814a91f2c194a
Tomche Delev <tomche.delev@sorsix.com>
2020-01-08 14:04:33 +0100

    Day 14 progress

--- Day14.hs
+++ Day14.hs
@@ -1,20 +1,25 @@
 module Day14 where
 
-import           Data.List
 import           Data.List.Split
 import           Data.Map        as M hiding (drop, filter, take)
 import           Debug.Trace
 
 toPair :: [String] -> ([String], String)
 toPair (x:y:_) = (splitOn ", " x, y)
+toPair _      = ([], "")
 
 toInt :: String -> Int
 toInt = read
 
 type Chemical = (Int, String)
 
+type ReactionMap = M.Map Chemical [Chemical]
+
+toReactionMap :: ([String], String) -> ReactionMap
+toReactionMap (xs, key) = M.insert (asChemical key) (fmap asChemical xs) M.empty
+
 asChemical :: String -> Chemical
-asChemical s = 
+asChemical s =
   let (a:b:_) = splitOn " " s
    in (toInt a, b)
 
@@ -22,5 +27,6 @@ main :: IO ()
 main = do
   input <- readFile "day14test.txt"
   let rows = lines input
-  let parts = fmap (toPair . splitOn " => ") rows
-  print $ fmap (asChemical . snd) parts
+  let pairs = fmap (toPair . splitOn " => ") rows
+  let reactions = fmap toReactionMap pairs
+  print $ reactions
