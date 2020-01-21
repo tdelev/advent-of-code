b2fdde3599d8e8ae662825ab330185e00d249f6c
Tomche Delev <tomce.delev@gmail.com>
2020-01-22 00:03:39 +0100

    Solved Day 17 part two <3

--- Day17.hs
+++ Day17.hs
@@ -6,7 +6,7 @@ import           Data.List
 import           Data.List.Split
 import           Data.Map        as M hiding (drop, filter, foldl, null, take)
 import           Data.Sequence   as S hiding (filter, length, null, replicate,
-                                       zip, zipWith)
+                                       reverse, zip, zipWith)
 import           Data.Set        (Set)
 import qualified Data.Set        as Set
 import           Debug.Trace
@@ -58,14 +58,6 @@ isIntersection view ((x, y), Scafold) =
   (view ! (x, y - 1) == Scafold) && (view ! (x, y + 1) == Scafold)
 isIntersection _ _ = False
 
-isPosIntersection :: View -> Position -> Bool
-isPosIntersection view position = isIntersection view (position, Scafold)
-
-isCrossroad :: View -> Position -> Bool
-isCrossroad view position =
-  let possible = explorePosition view position
-   in (length $ filter (\p -> view ! p == Scafold) possible) > 2
-
 drawTile :: Tile -> Char
 drawTile Open    = '.'
 drawTile Scafold = '#'
@@ -95,24 +87,6 @@ drawView view =
 sumAlignment :: [(Position)] -> Int
 sumAlignment xs = sum $ fmap (\(x, y) -> ((x - 1) * (y - 1))) xs
 
-type Visited = Set Position
-
-type IntersectionEntered = Set (Position, Position)
-
-type SearchState = (Visited, [Position], [Position])
-
-notVisited :: SearchState -> View -> Position -> Bool
-notVisited (visited, (current:_), _) view position = Set.notMember position visited
-
-notVisited' state view position =
-  trace
-    ("not visited : \nstate = " ++
-     (show state) ++ "\nposition = " ++ (show position))
-    notVisited
-    state
-    view
-    position
-
 inBounds :: View -> Position -> Bool
 inBounds view (x, y) =
   x >= 0 && x <= viewSize view && y >= 0 && y <= viewSize view
@@ -121,86 +95,117 @@ isValid :: View -> Position -> Bool
 isValid view position =
   inBounds view position && M.member position view && view ! position == Scafold
 
-plus :: Position -> Position -> Position
-plus (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
-
-explorePosition :: View -> Position -> [Position]
-explorePosition view position =
-  let filterFn = isValid view
-   in filter filterFn $ zipWith plus xyDelta $ replicate 4 position
-
-explorePosition' view position = trace ("explore position : pos = " ++ (show position)) explorePosition view position
-
 findVacuum :: View -> Position
 findVacuum view =
   fst $ head $ filter (\(_, tile) -> (fromEnum tile > 1)) $ M.toList view
 
-updateState :: SearchState -> View -> Position -> SearchState
-updateState (visited, current, intersection) view position =
-  (visited', position : current, intersection')
-  where
-    visited' = Set.insert position visited
-    intersection' =
-      if isCrossroad view position
-        then position:intersection
-        else intersection
-
-findPaths :: View -> SearchState -> Set Position -> [[Position]]
-findPaths view state dust =
-  let (visited, current, intersection) = state
-      position = head current
-      -- next = explorePosition view position
-      next = filter (notVisited state view) $ explorePosition view position
-   in if null next
-        then if isComplete current dust
-               then [current]
-               else if null intersection
-                      then []
-                      else findPaths
-                             view
-                             (visited, (head intersection) : current, tail intersection)
-                             dust
-        else let states = fmap (updateState state view) next
-              in concat $ fmap (\s -> findPaths view s dust) states
-
 asOutput :: String -> [Int]
 asOutput xs = fmap ord xs
 
-findDust :: View -> Set Position
-findDust view =
-  let dust = fmap fst $ filter (\(p, tile) -> tile == Scafold) $ M.toList view
-   in foldl (\s d -> Set.insert d s) Set.empty dust
+type Move = (Tile, Int)
 
-isComplete :: [Position] -> Set Position -> Bool
-isComplete [] set     = Set.null set
-isComplete (x:xs) set = isComplete xs $ Set.delete x set
+turn :: View -> Position -> Tile -> (Tile, Tile)
+turn view (x, y) tile =
+  let valid = isValid view
+   in case tile of
+        Up ->
+          if valid (x + 1, y)
+            then (Right, Right)
+            else if valid (x - 1, y)
+                   then (Left, Left)
+                   else (Open, Open)
+        Down ->
+          if valid (x - 1, y)
+            then (Right, Left)
+            else if valid (x + 1, y)
+                   then (Left, Right)
+                   else (Open, Open)
+        Right ->
+          if valid (x, y + 1)
+            then (Right, Down)
+            else if valid (x, y - 1)
+                   then (Left, Up)
+                   else (Open, Open)
+        Left ->
+          if valid (x, y - 1)
+            then (Right, Up)
+            else if valid (x, y + 1)
+                   then (Left, Down)
+                   else (Open, Open)
 
-isComplete' p set = trace ("is complete : \np = " ++ (show p)) isComplete p set
+turn' view position tile =
+  trace
+    ("turn : pos = " ++ (show position) ++ " ; tile = " ++ (show tile))
+    turn
+    view
+    position
+    tile
+
+advance :: View -> Position -> Tile -> (Position, Bool)
+advance view (x, y) direction =
+  let next =
+        case direction of
+          Up    -> (x, y - 1)
+          Right -> (x + 1, y)
+          Down  -> (x, y + 1)
+          Left  -> (x - 1, y)
+   in if isValid view next
+        then (next, True)
+        else ((x, y), False)
+
+vacuum :: View -> Position -> (Tile, Tile) -> [Move] -> [Move]
+vacuum view start (move, direction) [] =
+  let (position, success) = advance view start direction
+   in if success
+        then vacuum view position (move, direction) [(move, 1)]
+        else let tile' = turn view position direction
+              in vacuum view position tile' [(fst tile', 1)]
+vacuum view start (move, direction) moves =
+  let (position, success) = advance view start direction
+      ((move, count):xs) = moves
+   in if success
+        then vacuum view position (move, direction) ((move, count + 1) : xs)
+        else let (move', direction') = turn view position direction
+              in if move' == Open
+                   then moves
+                   else vacuum
+                          view
+                          position
+                          (move', direction')
+                          ((move', 0) : moves)
+
+moveToStr :: (Tile, Int) -> String
+moveToStr (Right, count) = "R" ++ (show count)
+moveToStr (Left, count)  = "L" ++ (show count)
+
+arrayToAscii :: [Char] -> [Int]
+arrayToAscii xs = (fmap ord xs) ++ [10]
 
 main :: IO ()
-main
-  -- input <- readFile "day17.txt"
-  -- let opcodes = getOpcodes input
-  -- let memory = load opcodes
-  -- let comp = boot memory
+main = do
+  input <- readFile "day17.txt"
+  let opcodes = getOpcodes input
+  let memory = load opcodes
+  let comp = boot memory
   -- let output = computerOutput $ run comp {computerState = Running}
-  -- print $ output
- = do
-  input <- readFile "day17test.txt"
-  let output = asOutput input
-  let view = asView output
-  let intersections = filter (isIntersection view) $ M.toList view
+  -- let view = asView output
+  -- let intersections = filter (isIntersection view) $ M.toList view
   -- putStrLn $ drawOutput output
-  print $ length intersections
-  putStrLn $ drawView view
-  let dust = findDust view
-  print $ dust --fmap (isCrossroad view) $ Set.toList dust
-  -- print $ isComplete [(13,8),(13,7),(13,6),(13,5),(12,5),(11,5),(11,4),(11,3),(11,2),(11,1),(12,1),(13,1),(14,1),(15,1),(15,2),(15,3),(15,4),(15,5),(15,6),(15,7),(15,8),(15,9),(14,9),(13,9),(13,10),(13,11),(12,11),(11,11),(10,11),(9,11),(9,10),(9,9),(8,9),(7,9),(7,8),(7,7),(6,7),(5,7),(4,7),(3,7),(2,7),(1,7)] dust
-  -- print $ sumAlignment $ fmap fst intersections
-  let start = findVacuum view
-  let first = head $ explorePosition view start
-  print $ first
-  print $ dust
-  let state = (Set.empty, [first], [])
-  -- print $ explorePosition view (9, 8)
-  print $ head $ findPaths view state dust
+  -- print $ length intersections
+  -- putStrLn $ drawView view
+  -- let start = findVacuum view
+  -- let move = (view ! start, view ! start)
+  -- let moves = vacuum view start move []
+  -- print $ concat $ fmap moveToStr $ reverse moves
+  let main = "A,B,A,A,B,C,B,C,C,B"
+  let a = "L,12,R,8,L,6,R,8,L,6"
+  let b = "R,8,L,12,L,12,R,8"
+  let c = "L,6,R,6,L,12"
+  let d = "n"
+  let input = concat $ fmap arrayToAscii [main, a, b, c, d]
+  let comp' = patchMemory comp 0 2
+  let output = computerOutput $ run comp' { 
+    computerState = Running,
+    computerInput = input
+  }
+  print $ output
