c6cdf1e917ad87e98a9da8d34dd71ab94d70a553
Tomche Delev <tomche.delev@sorsix.com>
2024-11-25 16:16:25 +0100

    more debugging

--- 2023/src/day_3.rs
+++ 2023/src/day_3.rs
@@ -1,9 +1,11 @@
 use std::{collections::HashSet, error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = fs::read_to_string("input/day_3.txt")?;
+    //let input = fs::read_to_string("input/day_3.txt")?;
+    let input = fs::read_to_string("input/day_3_sample.txt")?;
     // Part 1
     let symbols = symbols(input.as_str());
+    println!("{:?}", symbols);
     let result: i32 = input
         .lines()
         .enumerate()
@@ -13,8 +15,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .map(|np| (i as i32, *np))
                 .collect::<Vec<(i32, (i32, (i32, i32)))>>()
         })
-        .filter(|(i, (num, position))| touches_symbol(&symbols, position, i))
-        .map(|(_, (num, _))| num)
+        .filter(|(line_index, (_, position))| touches_symbol(&symbols, position, line_index))
+        .map(|(_, (num, _))| {
+            println!("num: {}", num);
+            num
+        })
         .sum();
     // Part 2
     println!("{}", result);
@@ -22,13 +27,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn touches_symbol(symbols: &HashSet<(i32, i32)>, position: &(i32, i32), line_index: &i32) -> bool {
+    println!("line index: {:?}", line_index);
+    println!("position: {:?}", position);
     let (start, end) = position;
-    let top_bottom: Vec<(i32, i32)> = (start - 1..end + 1)
-        .flat_map(|x| vec![(line_index - 1, x)])
+    let top_bottom: Vec<(i32, i32)> = (start - 1..=end + 1)
+        .flat_map(|x| vec![(line_index - 1, x), (line_index + 1, x)])
         .collect();
-    top_bottom.iter().any(|p| symbols.contains(p))
+    println!("top bottom: {:?}", top_bottom);
+    let touches = top_bottom.iter().any(|p| symbols.contains(p))
         || symbols.contains(&(*line_index, start - 1))
-        || symbols.contains(&(*line_index, end + 1))
+        || symbols.contains(&(*line_index, end + 1));
+
+    println!("touches: {}", touches);
+    touches
 }
 fn symbols(input: &str) -> HashSet<(i32, i32)> {
     input
@@ -37,7 +48,7 @@ fn symbols(input: &str) -> HashSet<(i32, i32)> {
         .flat_map(|(i, line)| {
             line.chars()
                 .enumerate()
-                .filter(|(j, c)| !c.is_digit(10) && *c != '.')
+                .filter(|(_, c)| !c.is_digit(10) && *c != '.')
                 .map(move |(j, _)| (i as i32, j as i32))
         })
         .collect()
@@ -52,12 +63,12 @@ fn numbers_from_line(line: &str) -> Vec<(i32, (i32, i32))> {
                     n *= 10;
                     n += c.to_digit(10).unwrap() as i32;
                     if i == line.len() - 1 {
-                        acc.push((n as i32, (start as i32, (i - 1) as i32)));
+                        acc.push((n as i32, (start as i32 - 1, (i - 1) as i32)));
                     }
                     (acc, n as i32, start as i32)
                 } else {
                     if n > 0 {
-                        acc.push((n as i32, (start as i32, (i - 1) as i32)));
+                        acc.push((n as i32, (start as i32 - 1, (i - 1) as i32)));
                     }
                     n = 0;
                     (acc, n as i32, i as i32)
