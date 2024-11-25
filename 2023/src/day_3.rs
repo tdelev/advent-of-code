8315149f745d31601c166f92cc3721b1cf616033
Tomche Delev <tomche.delev@sorsix.com>
2024-11-25 22:53:05 +0100

    day 3 progress

--- 2023/src/day_3.rs
+++ 2023/src/day_3.rs
@@ -1,46 +1,46 @@
 use std::{collections::HashSet, error::Error, fs};
+type Position = (i32, i32);
+type Range = (i32, i32);
+type Number = (i32, Range, i32);
 
 fn main() -> Result<(), Box<dyn Error>> {
-    //let input = fs::read_to_string("input/day_3.txt")?;
-    let input = fs::read_to_string("input/day_3_sample.txt")?;
+    let input = fs::read_to_string("input/day_3b_sample.txt")?;
+    //let input = fs::read_to_string("input/day_3_sample.txt")?;
     // Part 1
-    let symbols = symbols(input.as_str());
-    println!("{:?}", symbols);
-    let result: i32 = input
+    //let symbols = symbols(input.as_str());
+    //let result: i32 = input
+    //    .lines()
+    //    .enumerate()
+    //    .flat_map(|(i, line)| {
+    //        numbers_from_line(line)
+    //            .iter()
+    //            .map(|np| (i as i32, *np))
+    //            .collect::<Vec<(i32, Number)>>()
+    //    })
+    //    .filter(|(line_index, (_, position))| touches_symbol(&symbols, position, line_index))
+    //    .map(|(_, (num, _))| num)
+    //    .sum();
+    // Part 2
+    let numbers: Vec<(i32, Vec<(i32, i32)>)> = input
         .lines()
         .enumerate()
-        .flat_map(|(i, line)| {
-            numbers_from_line(line)
-                .iter()
-                .map(|np| (i as i32, *np))
-                .collect::<Vec<(i32, (i32, (i32, i32)))>>()
-        })
-        .filter(|(line_index, (_, position))| touches_symbol(&symbols, position, line_index))
-        .map(|(_, (num, _))| {
-            println!("num: {}", num);
-            num
-        })
-        .sum();
-    // Part 2
-    println!("{}", result);
+        .flat_map(|(i, line)| numbers_from_line(line, i as i32))
+        .map(|(n, (start, end), i)| (n, (start..=end).map(|j| (i, j)).collect()))
+        .collect();
+    println!("{:?}", numbers);
     Ok(())
 }
 
 fn touches_symbol(symbols: &HashSet<(i32, i32)>, position: &(i32, i32), line_index: &i32) -> bool {
-    println!("line index: {:?}", line_index);
-    println!("position: {:?}", position);
     let (start, end) = position;
     let top_bottom: Vec<(i32, i32)> = (start - 1..=end + 1)
         .flat_map(|x| vec![(line_index - 1, x), (line_index + 1, x)])
         .collect();
-    println!("top bottom: {:?}", top_bottom);
-    let touches = top_bottom.iter().any(|p| symbols.contains(p))
+    top_bottom.iter().any(|p| symbols.contains(p))
         || symbols.contains(&(*line_index, start - 1))
-        || symbols.contains(&(*line_index, end + 1));
-
-    println!("touches: {}", touches);
-    touches
+        || symbols.contains(&(*line_index, end + 1))
 }
+
 fn symbols(input: &str) -> HashSet<(i32, i32)> {
     input
         .lines()
@@ -53,25 +53,26 @@ fn symbols(input: &str) -> HashSet<(i32, i32)> {
         })
         .collect()
 }
-fn numbers_from_line(line: &str) -> Vec<(i32, (i32, i32))> {
+
+fn numbers_from_line(line: &str, row: i32) -> Vec<Number> {
     line.chars()
         .enumerate()
         .fold(
-            (Vec::new(), 0 as i32, 0),
+            (Vec::new(), 0i32, 0i32),
             |(mut acc, mut n, start), (i, c)| {
                 if c.is_digit(10) {
                     n *= 10;
                     n += c.to_digit(10).unwrap() as i32;
                     if i == line.len() - 1 {
-                        acc.push((n as i32, (start as i32 - 1, (i - 1) as i32)));
+                        acc.push((n, (start, i as i32), row));
                     }
-                    (acc, n as i32, start as i32)
+                    (acc, n, start)
                 } else {
                     if n > 0 {
-                        acc.push((n as i32, (start as i32 - 1, (i - 1) as i32)));
+                        acc.push((n, (start, i as i32 - 1), row));
                     }
                     n = 0;
-                    (acc, n as i32, i as i32)
+                    (acc, n, i as i32 + 1)
                 }
             },
         )
@@ -86,7 +87,7 @@ mod tests {
     #[test]
     fn test_numbers_from_line() {
         let actual = numbers_from_line("123...234...4546");
-        assert_eq!(actual, vec![(123, (0, 2)), (234, (5, 8)), (4546, (11, 14))]);
+        assert_eq!(actual, vec![(123, (0, 2)), (234, (6, 8)), (4546, (12, 15))]);
     }
 
     #[test]
