757345a1b289ec26f5b449758782387b80e087c7
Tomche Delev <tomche.delev@sorsix.com>
2024-11-26 15:35:47 +0100

    Day 4 part 1 done

---
+++ 2023/src/day_4.rs
@@ -0,0 +1,55 @@
+use std::{collections::HashSet, error::Error, fs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("input/day_4.txt")?;
+    // Part 1
+    let result: u32 = input
+        .lines()
+        .map(|line| {
+            let (_, numbers_part) = line.split_once(": ").unwrap();
+            let (winning, guesses) = numbers_part.split_once(" | ").unwrap();
+            let w: HashSet<u32> = numbers(winning.trim()).into_iter().collect();
+            let h: Vec<u32> = numbers(guesses.trim()).into_iter().collect();
+            points(w, h)
+        })
+        .sum();
+    println!("{}", result);
+    Ok(())
+}
+
+fn numbers(line: &str) -> Vec<u32> {
+    line.split(" ")
+        .filter(|n| !n.trim().is_empty())
+        .into_iter()
+        .map(|n| n.parse().unwrap())
+        .collect()
+}
+
+fn points(winning: HashSet<u32>, hand: Vec<u32>) -> u32 {
+    let count = hand.iter().filter(|n| winning.contains(n)).count() as u32;
+    if count > 0 {
+        2u32.pow(count - 1)
+    } else {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_numbers_from_line() {
+        let actual = numbers("1 2 3 4");
+        assert_eq!(actual, vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_points() {
+        let winning: HashSet<u32> = vec![41, 48, 83, 86, 17].into_iter().collect();
+        let hand: Vec<u32> = vec![83, 86, 6, 31, 17, 9, 48, 53];
+        let actual = points(winning, hand);
+        assert_eq!(actual, 8);
+    }
+}
