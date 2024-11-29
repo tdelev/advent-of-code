58a2d8db10965fd1e8e2889acc1db14697a6ab57
Tomche Delev <tomche.delev@sorsix.com>
2024-11-29 13:50:50 +0100

    Day 6 part 2 done

--- 2023/src/day_6.rs
+++ 2023/src/day_6.rs
@@ -1,19 +1,24 @@
-use std::{collections::HashSet, convert::TryInto, error::Error, fs, usize};
+use std::{convert::TryInto, error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input/day_6.txt")?;
     // Part 1
     let (times_line, distance_line) = input.split_once("\n").unwrap();
-    let times: Vec<u32> = line_to_numbers(times_line);
-    let distances = line_to_numbers(distance_line);
-    println!("times: {:?}, distance: {:?}", times, distances);
-    let result = times
-        .into_iter()
-        .zip(distances)
-        .map(|(time, record)| ways_to_beat(time, record))
-        .fold(1, |acc, i| acc * i);
-    println!("{}", result);
+    //let times: Vec<u32> = line_to_numbers(times_line);
+    //let distances = line_to_numbers(distance_line);
+    //println!("times: {:?}, distance: {:?}", times, distances);
+    //let result = times
+    //    .into_iter()
+    //    .zip(distances)
+    //    .map(|(time, record)| ways_to_beat(time, record))
+    //    .fold(1, |acc, i| acc * i);
+    //println!("{}", result);
     // Part 2
+    let time = line_to_number(times_line);
+    let distance = line_to_number(distance_line);
+    println!("t: {}, d: {}", time, distance);
+    let result = last_to_beat(time, distance) - first_to_beat(time, distance) + 1;
+    println!("{}", result);
     Ok(())
 }
 
@@ -26,6 +31,28 @@ fn ways_to_beat(time: u32, record: u32) -> u32 {
         .unwrap()
 }
 
+fn first_to_beat(time: u64, record: u64) -> u64 {
+    let mut t = 1u64;
+    while t < time {
+        if (time - t) * t > record {
+            return t;
+        }
+        t += 1;
+    }
+    return t;
+}
+
+fn last_to_beat(time: u64, record: u64) -> u64 {
+    let mut t = time;
+    while t > 0 {
+        if (time - t) * t >= record {
+            return t;
+        }
+        t -= 1;
+    }
+    return t;
+}
+
 fn line_to_numbers(line: &str) -> Vec<u32> {
     line.split_once(":")
         .unwrap()
@@ -34,6 +61,16 @@ fn line_to_numbers(line: &str) -> Vec<u32> {
         .split_whitespace()
         .map(|n| n.parse().unwrap())
         .collect()
+}
+
+fn line_to_number(line: &str) -> u64 {
+    line.split_once(":")
+        .unwrap()
+        .1
+        .trim()
+        .replace(" ", "")
+        .parse()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -51,5 +88,17 @@ mod tests {
     fn test_ways_to_beat_two() {
         let actual = ways_to_beat(15, 40);
         assert_eq!(actual, 8);
+    }
+
+    #[test]
+    fn test_first_to_beat() {
+        let actual = first_to_beat(71530, 940200);
+        assert_eq!(actual, 14);
+    }
+
+    #[test]
+    fn test_last_to_beat() {
+        let actual = last_to_beat(71530, 940200);
+        assert_eq!(actual, 71516);
     }
 }
