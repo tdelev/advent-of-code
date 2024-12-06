2e8607cc2a3b01129499936845bbfa61c24970fa
Tomche Delev <tomche.delev@sorsix.com>
2024-12-06 01:15:29 +0100

    Day 5 done

--- 2024/src/day_5.rs
+++ 2024/src/day_5.rs
@@ -1,15 +1,15 @@
 use std::{
+    cmp::Ordering,
     collections::{HashMap, HashSet},
     error::Error,
     fs,
-    iter::Map,
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input/day_5.txt")?;
     // Part 1
     let (orderings_input, printings) = input.split_once("\n\n").unwrap();
-    let orderings: HashMap<i32, HashSet<i32>> = orderings_input
+    let after_n: HashMap<i32, HashSet<i32>> = orderings_input
         .split_whitespace()
         .map(|l| {
             let (a, b) = l.split_once("|").unwrap();
@@ -20,7 +20,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             acc.entry(b).or_insert_with(HashSet::new).insert(a);
             acc
         });
-    let orderings_b: HashMap<i32, HashSet<i32>> = orderings_input
+    let before_n: HashMap<i32, HashSet<i32>> = orderings_input
         .split_whitespace()
         .map(|l| {
             let (a, b) = l.split_once("|").unwrap();
@@ -31,27 +31,46 @@ fn main() -> Result<(), Box<dyn Error>> {
             acc.entry(a).or_insert_with(HashSet::new).insert(b);
             acc
         });
+    //let result: i32 = printings
+    //    .lines()
+    //    .map(|line| line.split(",").map(|n| n.parse().unwrap()).collect())
+    //    .filter(|list| can_print(list, &after_n, &before_n))
+    //    .map(|list| *list.get(list.len() / 2).unwrap())
+    //    .sum();
+    //println!("{}", result);
+    // Part 2
+
     let result: i32 = printings
         .lines()
         .map(|line| line.split(",").map(|n| n.parse().unwrap()).collect())
-        .filter(|list| can_print(list, &orderings, &orderings_b))
+        .filter(|list| !can_print(list, &after_n, &before_n))
+        .map(|mut list| {
+            list.sort_by(|a, b| {
+                if after_n.get(b).map_or(false, |set| set.contains(a)) {
+                    Ordering::Less
+                } else if before_n.get(b).map_or(false, |set| set.contains(a)) {
+                    Ordering::Greater
+                } else {
+                    Ordering::Equal
+                }
+            });
+            list
+        })
         .map(|list| *list.get(list.len() / 2).unwrap())
         .sum();
     println!("{}", result);
-    // Part 2
-
     Ok(())
 }
 
 fn can_print(
     list: &Vec<i32>,
-    orderings: &HashMap<i32, HashSet<i32>>,
-    orderings_b: &HashMap<i32, HashSet<i32>>,
+    after_n: &HashMap<i32, HashSet<i32>>,
+    before_n: &HashMap<i32, HashSet<i32>>,
 ) -> bool {
     list.iter().enumerate().all(|(i, n)| {
         list.iter().skip(i + 1).all(|after| {
-            orderings.get(after).map_or(true, |set| set.contains(n))
-                && orderings_b.get(after).map_or(true, |set| !set.contains(n))
+            after_n.get(after).map_or(true, |set| set.contains(n))
+                && before_n.get(after).map_or(true, |set| !set.contains(n))
         })
     })
 }
